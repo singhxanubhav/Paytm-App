0d1108a3fcb2fab41372707c76bfa232483c268d
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-03-18 01:18:23 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import { connectDb } from "./models/index.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "*",
     methods: ['POST', 'GET', 'DELETE', 'PUT', 'OPTIONS'],
     credentials: true
 }));
